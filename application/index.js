a02c8c7ec5565daebd648b73105837ca476d023d
Taisiia Sherstiukova <62242543+tayasherstyukova@users.noreply.github.com>
2022-11-02 16:56:13 -0600

    initial set up

---
+++ application/index.js
@@ -0,0 +1,167 @@
+const express = require('express');
+const app = express();
+const pgp = require('pg-promise')();
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const axios = require('axios');
+
+// database configuration
+const dbConfig = {
+    host: 'db',
+    port: 5432,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+  };
+  
+  const db = pgp(dbConfig);
+  // console.log("Api key", process.env.API_KEY)
+
+
+  // test your database
+  db.connect()
+    .then(obj => {
+      console.log('Database connection successful'); // you can view this message in the docker compose logs
+      obj.done(); // success, release the connection;
+    })
+    .catch(error => {
+      console.log('ERROR:', error.message || error);
+    });
+
+    app.set('view engine', 'ejs');
+    app.use(bodyParser.json());
+    app.use(
+        session({
+          secret: process.env.SESSION_SECRET,
+          saveUninitialized: false,
+          resave: false,
+        })
+      );
+      
+      app.use(
+        bodyParser.urlencoded({
+          extended: true,
+        })
+      );
+
+      app.get('/', (req, res) =>{
+        res.redirect('/discover'); 
+      });
+
+    
+      app.get('/login', (req, res) => {
+        res.render('pages/login');
+      });
+
+    app.get('/register', (req, res) => {
+        res.render('pages/register');
+      });
+
+
+
+    app.post('/register', async (req, res) => {
+        //the logic goes here
+        const username = req.body.username;
+        const password = req.body.password;
+        const hash = await bcrypt.hash(password, 10);
+        const query = 'insert into users (username, password) values ($1, $2) returning *';
+    db.any(query, [
+      username,
+      hash
+    ])
+      .then(() =>{
+       res.redirect('/login');
+
+      })
+      .catch(err=>{
+        console.log(err);
+        res.redirect("/register");
+      });
+    });
+
+    
+    app.post('/login', async (req, res) => {
+        //the login goes here
+        const username = req.body.username;
+        const password = req.body.password;
+        const query = `select * from users where username = '${username}'`;
+        db.any(query)
+        .then(async data =>{
+        const match = await bcrypt.compare(password, data[0].password); 
+        if(match){
+            req.session.user = {
+                api_key: process.env.API_KEY,
+                };
+                req.session.save();
+                res.redirect('/discover');
+        }
+        else{
+            res.render("pages/register",{
+                message: "Incorrect username or password",
+                error:true
+            })
+        }
+
+        })
+        .catch(err=>{
+            
+            res.render("pages/login",{
+                message: "Database Request Failed",
+                error:true
+            })
+        });
+    });
+
+    // Authentication Middleware.
+const auth = (req, res, next) => {
+    console.log(req.session)
+    if (!req.session.user) {
+      // Default to register page.
+      return res.redirect('/register');
+    }
+    next();
+  };
+  // Authentication Required
+  app.use(auth);
+
+  app.get('/discover', (req, res) =>{
+    axios({
+   url: `https://app.ticketmaster.com/discovery/v2/events.json`,
+      method: 'GET',
+      dataType:'json',
+      params: {
+          "apikey": req.session.user.api_key,
+          "keyword": "The Backseat Lovers" ,//you can choose any artist/event here
+          "size": 10,
+      }
+   })
+   .then(results => {
+      console.log(results.data._embedded.events[0]); // the results will be displayed on the terminal if the docker containers are running
+     // Send some parameters
+      res.render("pages/discover",{
+        results: results.data._embedded.events
+      });
+   })
+   .catch(error => {
+    // Handle errors
+      res.render("pages/discover",{
+      results: [],
+      message: "Incorrect username or password",
+      error:true
+  })
+
+ });
+
+  });
+
+app.get('/logout', (req, res) => {
+  req.session.destroy();
+    res.render('pages/login',{
+        message: "Logged out Successfully"
+    })
+  });
+
+  app.listen(3000);
+  console.log("Server is listening on port 3000");
+      
